8a65f5fd6582166c6b3611a5c59bd04ed246a9c0
Salih Komut <salih.komut@student.ap.be>
2023-07-11 18:06:15 +0200

    added a refresh button , also added the design for it in css

--- my-weather-app/src/Weather.js
+++ my-weather-app/src/Weather.js
@@ -3,21 +3,33 @@ import './styles.css';
 import { Card } from 'semantic-ui-react'
 import moment from 'moment';
 
-const CardExampleCard = ({weatherData}) => (
-    <div className="main">
-        <p className="header">{weatherData.name}</p>
-        <div className="flex">
-          <p className="day">Day: {moment().format('dddd')}</p>
-          <p className="day">{moment().format('LL')}</p>
-        </div>
+const refresh = () => {
+    window.location.reload();
+}
 
-    <div className="flex">
-      <p className="temp">Temprature: {weatherData.main.temp} &deg;C</p>
-      <p className="temp">Humidity: {weatherData.main.humidity} %</p>
+const WeatherCard = ({weatherData}) => (
+    <div className="main">
+  
+        <div className="top">
+          <p className="header">{weatherData.name}</p>
+          <Button className="button" inverted color='blue' circular icon='refresh' onClick={refresh} />
+        </div>
+        <div className="flex">
+          <p className="day">{moment().format('dddd')}, <span>{moment().format('LL')}</span></p>
+          <p className="description">{weatherData.weather[0].main}</p>
+        </div>
+  
+        <div className="flex">
+          <p className="temp">Temprature: {weatherData.main.temp} &deg;C</p>
+          <p className="temp">Humidity: {weatherData.main.humidity} %</p>
+        </div>
+  
+        <div className="flex">
+          <p className="sunrise-sunset">Sunrise: {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-IN')}</p>
+          <p className="sunrise-sunset">Sunset: {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN')}</p>
+        </div>
+      
     </div>
-    
-    
-</div>
-)
+  )
 
 export default CardExampleCard;
